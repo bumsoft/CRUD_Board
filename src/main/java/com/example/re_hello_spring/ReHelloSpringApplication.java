509eef84d8210cd1859c5b9c6b808a6dabc9d669
신진범 <shjb07015@gmail.com>
2024-07-07 14:06:42 +0900

    Initial commit

---
+++ src/main/java/com/example/re_hello_spring/ReHelloSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.re_hello_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReHelloSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReHelloSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/re_hello_spring/ReHelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.re_hello_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReHelloSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
